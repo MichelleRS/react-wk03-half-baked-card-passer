dbc946c20021e1ad01cf75f975711cf96953dfb3
Michelle Ross <miross629@gmail.com>
2023-01-25 14:55:12 -0800

    Refactor selected card

---
+++ src/context/GameContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState('');
+
+  return (
+    <GameContext.Provider value={{ selectedCard, setSelectedCard }}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  return context;
+};
+
+export { GameProvider, useGameContext };
